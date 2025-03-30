166da5b3ab22556662404a6bf156565e6ff1daa8
mytopgraf <mytopgraf@gmail.com>
2025-03-30 07:51:08 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,33 @@
 import nodemailer from "nodemailer";
 
-export default async ({ req, res }) => {
+export default async ({ req, res, context }) => {
     try {
+        context.log("Parsing request body...");
         const { email, subject, message } = JSON.parse(req.body);
+        context.log("Email details parsed:", { email, subject, message });
 
-		const transport = nodemailer.createTransport({
-		  host: "sandbox.smtp.mailtrap.io",
-		  port: 2525,
-		  auth: {
-            user: process.env.MAILTRAP_USER,  // Логин из Mailtrap
-            pass: process.env.MAILTRAP_PASS   // Пароль из Mailtrap
-		  }
-		});
+        const transport = nodemailer.createTransport({
+            host: "sandbox.smtp.mailtrap.io",
+            port: 2525,
+            auth: {
+                user: process.env.MAILTRAP_USER,  // Логин из Mailtrap
+                pass: process.env.MAILTRAP_PASS   // Пароль из Mailtrap
+            }
+        });
 
-        await transporter.sendMail({
-            from: `"Test App" <test@app.com>`,  // От кого (можно любой email)
-            to: user: process.env.EMAIL_USER,  // Кому
+        context.log("Transporter created. Sending email...");
+
+        await transport.sendMail({
+            from: "<test@gmail.com>",  
+            to: process.env.EMAIL_USER,  // Кому
             subject: "Hi test",
             text: "Hi test"
         });
 
+        context.log("Email sent successfully!");
         return res.json({ success: true, message: "Email sent successfully!" });
-
     } catch (error) {
+        context.log("Error sending email:", error.message);
         return res.json({ success: false, error: error.message });
     }
 };
